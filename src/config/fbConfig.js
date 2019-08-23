a060ac5fbdf4279e62516716a6fd0531a8c13784
Siddhant Misra <msiddhant22@gmail.com>
2019-08-23 15:54:04 -0400

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,13 +5,7 @@ import 'firebase/auth';
 // Replace this with your own config details
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyANy1IVhtlTiTA1VIcLC01WKEtGtVdpcPQ",
-  authDomain: "sidd-dashboard.firebaseapp.com",
-  databaseURL: "https://sidd-dashboard.firebaseio.com",
-  projectId: "sidd-dashboard",
-  storageBucket: "sidd-dashboard.appspot.com",
-  messagingSenderId: "664821875572",
-  appId: "1:664821875572:web:166aec91a5e9c550"
+  "insert your firebase credentials here"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
